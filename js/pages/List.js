1807bdca66cdb988e0e983c626c41d727184977d
kiwiflare <thekiwiflare@gmail.com>
2026-08-03 16:08:35 +0200

    changed formatting

--- js/pages/List.js
+++ js/pages/List.js
@@ -99,7 +99,7 @@ export default {
                         </ol>
                     </template>
                     <h3>Submission Requirements</h3>
-					<p><b>Basic Guidelines</b></p>
+					<h5>Basic Guidelines</h5>
                     <p>
                         The completion must be done on the level that is listed on the site.
                     </p>
@@ -115,7 +115,7 @@ export default {
                     <p>
                         The recording must show the ending screen (attempts, jumps, etc.)
                     </p>
-					<p><b>Skips/Secret ways</b></p>
+					<h5>Skips/Secret ways</h5>
                     <p>
                         If the completion uses a skip that significantly lowers the difficulty of the level (e.g. missing a fast speed portal in a wave part), it is up to the creator of the level to patch and disallow it.
                     </p>
@@ -125,7 +125,7 @@ export default {
 					<p>
 						- If they don't, the completion will be accepted, and the level's placement will be re-evaluated accounting for the skip.
 					</p>
-					<p><b>Level Position</b></p>
+					<h5>Level Position</h5>
                     <p>
                         Once a level falls into the Legacy list (out of the top 69), records for it will still be accepted until 24 hours later.
                     </p>
