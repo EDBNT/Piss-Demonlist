475dab38d8ae60d39025f96cc9fa9bae41b845e7
KrisGra <39800337+krisgrant@users.noreply.github.com>
2026-08-04 01:57:47 +0300

    Update leaderboard to not display 0 points users

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -26,19 +26,21 @@ export default {
                 </div>
                 <div class="board-container">
                     <table class="board">
-                        <tr v-for="(ientry, i) in leaderboard">
-                            <td class="rank">
-                                <p class="type-label-lg">#{{ i + 1 }}</p>
-                            </td>
-                            <td class="total">
-                                <p class="type-label-lg">{{ localize(ientry.total) }}</p>
-                            </td>
-                            <td class="user" :class="{ 'active': selected == i }">
-                                <button @click="selected = i">
-                                    <span class="type-label-lg">{{ ientry.user }}</span>
-                                </button>
-                            </td>
-                        </tr>
+                        <template v-for="(ientry, i) in leaderboard">
+                            <tr v-if="ientry.total > 0">
+                                <td class="rank">
+                                    <p class="type-label-lg">#{{ i + 1 }}</p>
+                                </td>
+                                <td class="total">
+                                    <p class="type-label-lg">{{ localize(ientry.total) }}</p>
+                                </td>
+                                <td class="user" :class="{ 'active': selected == i }">
+                                    <button @click="selected = i">
+                                        <span class="type-label-lg">{{ ientry.user }}</span>
+                                    </button>
+                                </td>
+                            </tr>
+                        </template>
                     </table>
                 </div>
                 <div class="player-container">
